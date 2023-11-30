cc376a6d96e9e0d7e1e413bbc06efcf641902347
oskarus <39172595+oskarus@users.noreply.github.com>
2023-11-30 09:10:14 +0100

    fix: special-offer method

--- app/routers/special_offers.py
+++ app/routers/special_offers.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
 from services.special_offers import SpecialOfferService
 from routers.models import ProductSpecialOfferResponse
 
@@ -11,6 +10,6 @@ async def get_products_special_offers():
     return await SpecialOfferService.get_products_special_offers()
 
 
-@router.post("/special-offers/export-csv", response_class=FileResponse)
+@router.post("/special-offers/export-csv")
 async def export_special_offers_csv():
-    return await SpecialOfferService.export_scpecial_offers_csv()
+    return await SpecialOfferService.export_special_offers_csv()
