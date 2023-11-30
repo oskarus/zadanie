72c951aa9a13dc964711fb623850b07a56f0b80e
oskarus <39172595+oskarus@users.noreply.github.com>
2023-11-30 08:58:39 +0100

    feat: adjust special-offers functionality

--- app/services/special_offers.py
+++ app/services/special_offers.py
@@ -1,10 +1,10 @@
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
-from database import async_session
+from db.utils import async_session
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 from db.models import Laptop, PC, Printer
-from routers.model import LaptopDetails, PCDetails, PrinterDetails
+from routers.models import LaptopDetails, PCDetails, PrinterDetails
 from decimal import Decimal
 import csv
 
@@ -40,64 +40,64 @@ async def fetch_single_product(session, model):
 class SpecialOfferService:
     @staticmethod
     async def get_products_special_offers() -> JSONResponse:
-    try:
-        async with async_session() as session:
-            pc = await fetch_single_product(session, PC)
-            laptop = await fetch_single_product(session, Laptop)
-            printer = await fetch_single_product(session, Printer)
+        try:
+            async with async_session() as session:
+                pc = await fetch_single_product(session, PC)
+                laptop = await fetch_single_product(session, Laptop)
+                printer = await fetch_single_product(session, Printer)
 
-            if not (pc and laptop and printer):
-                raise HTTPException(status_code=404, detail="Insufficient products for special offers")
+                if not (pc and laptop and printer):
+                    raise HTTPException(status_code=404, detail="Insufficient products for special offers")
 
-            special_offers = [
-                create_special_offer('PC + Printer', pc, printer),
-                create_special_offer('Laptop + Printer', laptop, printer)
-            ]
+                special_offers = [
+                    create_special_offer('PC + Printer', pc, printer),
+                    create_special_offer('Laptop + Printer', laptop, printer)
+                ]
 
-            return JSONResponse(status_code=200, content={"special_offers": special_offers})
-    except HTTPException as e:
-        return JSONResponse(status_code=e.status_code, content={"error": e.detail})
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"error": str(e)})
+                return JSONResponse(status_code=200, content={"special_offers": special_offers})
+        except HTTPException as e:
+            return JSONResponse(status_code=e.status_code, content={"error": e.detail})
+        except Exception as e:
+            return JSONResponse(status_code=500, content={"error": str(e)})
 
     @staticmethod
     async def export_special_offers_csv() -> JSONResponse:
-    try:
-        async with async_session() as session:
-            pc = await fetch_single_product(session, PC)
-            laptop = await fetch_single_product(session, Laptop)
-            printer = await fetch_single_product(session, Printer)
+        try:
+            async with async_session() as session:
+                pc = await fetch_single_product(session, PC)
+                laptop = await fetch_single_product(session, Laptop)
+                printer = await fetch_single_product(session, Printer)
 
-            if not (pc and laptop and printer):
-                raise HTTPException(status_code=404, detail="Insufficient products for special offers")
+                if not (pc and laptop and printer):
+                    raise HTTPException(status_code=404, detail="Insufficient products for special offers")
 
-            special_offers = [
-                create_special_offer('PC + Printer', pc, printer),
-                create_special_offer('Laptop + Printer', laptop, printer)
-            ]
+                special_offers = [
+                    create_special_offer('PC + Printer', pc, printer),
+                    create_special_offer('Laptop + Printer', laptop, printer)
+                ]
 
-            csv_file_path = "outputs/special_offers.csv"
+                csv_file_path = "outputs/special_offers.csv"
 
-            with open(csv_file_path, mode='w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(['Promotion Set', 'Products', 'Products Details', 'Discounted Price'])
+                with open(csv_file_path, mode='w', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(['Promotion Set', 'Products', 'Products Details', 'Discounted Price'])
 
-                for special_offer in special_offers:
-                    product_names = ', '.join(special_offer['products'].keys())
-                    product_details = '; '.join(
-                        f"{name} - " + ', '.join(f'{key}: {value}' for key, value in details.items())
-                        for name, details in special_offer['products'].items()
-                    )
-                    writer.writerow([
-                        special_offer['special_offer_set'],
-                        product_names,
-                        product_details,
-                        special_offer['discounted_price']
-                    ])
+                    for special_offer in special_offers:
+                        product_names = ', '.join(special_offer['products'].keys())
+                        product_details = '; '.join(
+                            f"{name} - " + ', '.join(f'{key}: {value}' for key, value in details.items())
+                            for name, details in special_offer['products'].items()
+                        )
+                        writer.writerow([
+                            special_offer['special_offer_set'],
+                            product_names,
+                            product_details,
+                            special_offer['discounted_price']
+                        ])
 
-            return JSONResponse(status_code=200, content={"filepath": csv_file_path})
+                return JSONResponse(status_code=200, content={"filepath": csv_file_path})
 
-    except HTTPException as e:
-        return JSONResponse(status_code=e.status_code, content={"error": e.detail})
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"error": str(e)})
+        except HTTPException as e:
+            return JSONResponse(status_code=e.status_code, content={"error": e.detail})
+        except Exception as e:
+            return JSONResponse(status_code=500, content={"error": str(e)})
