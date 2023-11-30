6df2ee182cb90734f66362182b8205597f16a346
oskarus <39172595+oskarus@users.noreply.github.com>
2023-11-30 08:53:40 +0100

    feat: improve endpoints

--- app/routers/special_offers.py
+++ app/routers/special_offers.py
@@ -6,11 +6,11 @@ from routers.models import ProductSpecialOfferResponse
 router = APIRouter(prefix="/api/products")
 
 
-@router.get("/special_offers", response_model=ProductSpecialOfferResponse)
+@router.get("/special-offers", response_model=ProductSpecialOfferResponse)
 async def get_products_special_offers():
     return await SpecialOfferService.get_products_special_offers()
 
 
-@router.post("/special_offers/export-csv", response_class=FileResponse)
+@router.post("/special-offers/export-csv", response_class=FileResponse)
 async def export_special_offers_csv():
     return await SpecialOfferService.export_scpecial_offers_csv()
